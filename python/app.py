c911e21ca7ae4cab05d1fbfbff2137083e157d8c
Brandon Fallin <bmfallin@gmail.com>
2020-08-29 08:31:29 -0400

    Broke out css files

--- python/app.py
+++ python/app.py
@@ -2,10 +2,25 @@ import dash
 import data
 
 # Define the stylesheet
-external_stylesheets = [".\\static\\stylesheet.css"]
+external_stylesheets = [
+    ".\\assets\\stylesheets\\stylesheet.css",
+    ".\\assets\\stylesheets\\buttons.css",
+    ".\\assets\\stylesheets\\data-table.css",
+    ".\\assets\\stylesheets\\date-range.css",
+    ".\\assets\\stylesheets\\navigation.css",
+    ".\\assets\\stylesheets\\select-menu.css",
+    ".\\assets\\stylesheets\\sliders.css",
+    ".\\assets\\stylesheets\\spinners.css"
+]
 
 # Define the dash app
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True
+)
+
+app.title="Cheapeake Bay Watershed Analysis"
 
 # Load our dataframes
 huc_gaps_df = data.load_huc_gaps()
